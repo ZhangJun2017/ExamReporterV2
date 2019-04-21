040691431a8b5f27292e18c4e5c7b36479547268
ZhangJun-ZhangJun_PC <sn2015.zhang@gmail.com>
2019-04-21 15:17:22 +0800

    Give it up.Let's do it next week.

--- app/src/main/java/sn/zhang/exam_reporter_v2/MainActivity.java
+++ app/src/main/java/sn/zhang/exam_reporter_v2/MainActivity.java
@@ -6,16 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
 
+import flex.messaging.io.amf.ASObject;
+import flex.messaging.io.amf.client.AMFConnection;
+import sn.zhang.amfparser.Config;
+import sn.zhang.amfparser.utils.Tools;
+import sn.zhang.amfparser.utils.values;
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.a_main);
+        try {
+            new Config().main(null);
+        } catch (Exception e) {
 
+        }
+        setSupportActionBar((Toolbar) (findViewById(R.id.toolbar)));
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +34,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void query() {
+        values values = new values();
+        Tools tools = new Tools();
 
+        try {
+
+            AMFConnection amfConnection = new AMFConnection();
+            amfConnection.connect(values.url);
+            Object result1 = amfConnection.call(values.command, new Object[]{19868, values.studentID, "Why not check my token???"});
+            ASObject asObject = (ASObject) result1;
+            ArrayList rootMap = (ArrayList) asObject.get("source");
+            tools.query(rootMap, asObject, values);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
+
